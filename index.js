f1c233bbcf6d044c8b1e4fc9a5a26ddeb469a660
KYLE MOLIDOR <kyle_molidor@yahoo.com>
2021-01-08 06:01:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function happyHolidays() {
 function happyHolidaysTo(name) {
   return `Happy holidays, ${name}!`;
 }
-function happyHolidayTo(holiday, name) {
+function happyCustomHolidayTo(holiday, name) {
   return `Happy ${holiday}, ${name}!`;
 }
 function holidayCountdown(holiday, days) {
